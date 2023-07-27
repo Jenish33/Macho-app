f84d8fb0e68e84828a59d451cfaf557ac60a77ea
Jenish <samueljenish33@gmail.com>
2023-07-27 22:22:41 +0530

    :zap: Video Upload Page Created

---
+++ Front-End/src/components/PushUpCounterForm.jsx
@@ -0,0 +1,77 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { Upload, Button, message, Typography } from 'antd'
+import { InboxOutlined } from '@ant-design/icons'
+import {useState} from 'react'
+import axios from 'axios'
+import './PushUpCounterForm.css'
+
+const PushUpCounterForm = () => {
+	const { Title } = Typography;
+	const { Dragger } = Upload;
+	const [videoFile, setVideoFile] = useState(null);
+	const [result, setResult] = useState('')
+
+	const handleVideoChange = (info) => {
+    // We only allow a single file to be uploaded
+    const fileList = info.fileList.slice(-1);
+
+    // Update the state with the selected video file
+    setVideoFile(fileList[0].originFileObj);
+  };
+
+	const handleUpload = async () => {
+		try {
+			if(!videoFile) {
+				message.error('Please upload a video file first');
+				return;
+			}
+
+			const formData = new FormData();
+			formData.append('video', videoFile);
+			setResult('Processing...');
+
+			const response = await axios.post('http://localhost:3001/upload/', formData);
+
+			if (response.data) {
+        message.success('Processed successfully');
+        setResult(response.data.push_up_count);
+      } else {
+        message.error('Failed to get the push-up count');
+      }
+		} catch (err) {
+				console.log(err);
+				message.error('An Error occurred while processing the video.');
+		}
+	}
+
+
+
+  return (
+	<div className='pushUpCounterForm'>
+		<Title style={{color: 'white', fontSize: '35px'}}>Push Up Counter App</Title>
+		<Dragger
+			customRequest={()=> {}} //Prevent automatic upload
+			fileList={videoFile ? [videoFile] : []}
+			onChange={handleVideoChange}
+			onRemove={() => setVideoFile(null)}
+			className='dragger'
+		>
+  		<p className="ant-upload-drag-icon dragger_p">
+  		  <InboxOutlined />
+  		</p>
+  		<p className="ant-upload-text dragger_p">Click or drag file to this area to upload</p>
+  		<p className="ant-upload-hint dragger_p">
+  		  We need your PushUp Video to be uploaded here
+  		</p>
+  	</Dragger>
+
+		<Button className='submit-btn' onClick={() => handleUpload()} >
+			Process Video
+		</Button>
+		{result && <h1 style={{color: 'white'}}>Push-up count: {result}</h1>}
+	</div>
+    
+  )
+}
+
+export default PushUpCounterForm
